da316ebc62466206433eae60c78e62a0ea08d96f
nina <ninaw1@uci.edu>
2019-11-14 09:21:54 -0800

    Updates

---
+++ precache-manifest.f4f4f89442a3ef50ef80e2385cfba338.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c7ca92b379d041f302b5015cc11ed70",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "a16c5208d602413b828f",
+    "url": "/my-portfolio/static/css/2.04aae6c4.chunk.css"
+  },
+  {
+    "revision": "bc7a0505337a9f5ce126",
+    "url": "/my-portfolio/static/css/main.3fc1eb3e.chunk.css"
+  },
+  {
+    "revision": "a16c5208d602413b828f",
+    "url": "/my-portfolio/static/js/2.1324adfa.chunk.js"
+  },
+  {
+    "revision": "bc7a0505337a9f5ce126",
+    "url": "/my-portfolio/static/js/main.558869a5.chunk.js"
+  },
+  {
+    "revision": "39485d276cb6a495935f",
+    "url": "/my-portfolio/static/js/runtime-main.1cf6ada0.js"
+  },
+  {
+    "revision": "ddaa01de3b45c40bc4d72e0a736e6e28",
+    "url": "/my-portfolio/static/media/Snapseed.ddaa01de.JPG"
+  },
+  {
+    "revision": "73ca02053949ac29f9a71c4232f03b03",
+    "url": "/my-portfolio/static/media/aboutme.73ca0205.jpg"
+  },
+  {
+    "revision": "72854ad1a7be1780b83efda4f9063c2a",
+    "url": "/my-portfolio/static/media/art_1.72854ad1.jpg"
+  },
+  {
+    "revision": "a91ae0b56ed2fed73bc71e13ba6e9592",
+    "url": "/my-portfolio/static/media/art_10.a91ae0b5.jpg"
+  },
+  {
+    "revision": "1749dbf6b0026d8b0631e9543effad3d",
+    "url": "/my-portfolio/static/media/art_11.1749dbf6.jpg"
+  },
+  {
+    "revision": "63ef5989239ee77323bc7dec971195d4",
+    "url": "/my-portfolio/static/media/art_12.63ef5989.jpg"
+  },
+  {
+    "revision": "a54640829b1f67c41452a7e11651f00b",
+    "url": "/my-portfolio/static/media/art_13.a5464082.jpg"
+  },
+  {
+    "revision": "c03decbf0ce68fc01d629bd656ab1e1f",
+    "url": "/my-portfolio/static/media/art_14.c03decbf.jpg"
+  },
+  {
+    "revision": "bf960d215f39854f768b0e5997c73a0b",
+    "url": "/my-portfolio/static/media/art_15.bf960d21.jpg"
+  },
+  {
+    "revision": "06f52f99f41c20f0b54006e29edf23a1",
+    "url": "/my-portfolio/static/media/art_16.06f52f99.jpg"
+  },
+  {
+    "revision": "d5cff6da91b0c0d8b4fa9d32886ce005",
+    "url": "/my-portfolio/static/media/art_17.d5cff6da.jpg"
+  },
+  {
+    "revision": "ffedb9608e252e513e9286f272849082",
+    "url": "/my-portfolio/static/media/art_18.ffedb960.jpg"
+  },
+  {
+    "revision": "aae0cefc8226b1e181ed3313bb6167ee",
+    "url": "/my-portfolio/static/media/art_2.aae0cefc.jpg"
+  },
+  {
+    "revision": "6d497b32521aa1feffee9535805439fa",
+    "url": "/my-portfolio/static/media/art_3.6d497b32.jpg"
+  },
+  {
+    "revision": "fcdc39342aaab444831fd0aee1f11713",
+    "url": "/my-portfolio/static/media/art_4.fcdc3934.jpg"
+  },
+  {
+    "revision": "ea8feefdf685ba4560ea752d991517e8",
+    "url": "/my-portfolio/static/media/art_5.ea8feefd.jpg"
+  },
+  {
+    "revision": "c60fbaf855e25fa5ac03592217996f3c",
+    "url": "/my-portfolio/static/media/art_6.c60fbaf8.jpg"
+  },
+  {
+    "revision": "117f05802c8561bea96e388f93cb8df5",
+    "url": "/my-portfolio/static/media/art_7.117f0580.jpg"
+  },
+  {
+    "revision": "5038eeea2aee06873abc26466e7bc5e2",
+    "url": "/my-portfolio/static/media/art_8.5038eeea.jpg"
+  },
+  {
+    "revision": "37e293f1b1387e477bd7bb6cb7321bef",
+    "url": "/my-portfolio/static/media/art_9.37e293f1.jpg"
+  },
+  {
+    "revision": "bed6328816a9e822da7d32e63f242c27",
+    "url": "/my-portfolio/static/media/avatar.bed63288.JPG"
+  }
+]);
